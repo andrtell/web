487f1b788d7d0552a2177efbc617a16c4d87f0f3
Andreas Tell <git@tell.nu>
2022-10-26 11:03:19 +0200

    add game of life to index page

--- lib/tell_web/live/index_live.ex
+++ lib/tell_web/live/index_live.ex
@@ -1,7 +1,20 @@
 defmodule TellWeb.IndexLive do
   use TellWeb, :live_view
 
+  alias Conway.Cell
+  alias Conway.World
+
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    cells =
+      for y <- 0..(World.height() - 1), x <- 0..(World.width() - 1) do
+        %{
+          alive?: Cell.alive?({x, y}),
+          x: x,
+          y: y,
+          dot: Integer.mod(x + y, 6) + 1
+        }
+      end
+
+    {:ok, assign(socket, cells: cells)}
   end
 end
