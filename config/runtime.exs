66901086ca72eba8056aa84cb8fb309692dca3a5
Andreas Tell <git@tell.nu>
2022-10-26 11:11:09 +0200

    add ssl in prod

--- config/runtime.exs
+++ config/runtime.exs
@@ -25,18 +25,14 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
-  port = String.to_integer(System.get_env("PORT") || "4000")
-
-  config :tell, TellWeb.Endpoint,
-    url: [host: host, port: 443],
-    http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
+  config :web, SiteWeb.Endpoint,
+    url: [scheme: "https", host: "tell.nu", port: 443],
+    http: [port: 80],
+    https: [
+      port: 443,
+      cipher_suite: :strong,
+      keyfile: System.get_env("SSL_KEY_PATH"),
+      certfile: System.get_env("SSL_CERT_PATH")
     ],
     secret_key_base: secret_key_base
 
